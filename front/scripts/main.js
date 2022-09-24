798971bbaace89307a3d5aae8f54b524eee02ee9
Dan <danstolyarov79@gmail.com>
2022-09-24 11:40:41 +0300

    works good

--- front/scripts/main.js
+++ front/scripts/main.js
@@ -12,7 +12,75 @@ window.addEventListener("load", async () =>
     {  
         videoContainer.classList.add('active');
         fileInputLabel.classList.remove('active');
+
         video.src = URL.createObjectURL(fileInput.files[0]);
+        video.onloadedmetadata = (ev) =>
+        {
+            document.querySelector('#time-end').value = (new Date(+video.duration*1000 + (new Date()).getTimezoneOffset() * 60000)).toTimeString().split(' ')[0];
+            document.querySelector('#time-start').addEventListener("change", (event)=>
+            {
+                const listTime = event.target.value.split(':');
+                let seconds = (+listTime[0]) * 3600 + (+listTime[1]) * 60 + (+listTime[2]);
+                if (seconds > video.duration)
+                {
+                    seconds = video.duration - 1;
+                    const date = (new Date(seconds*1000 + (new Date()).getTimezoneOffset() * 60000)).toTimeString().split(' ')[0];
+                    event.target.value = date;
+                }
+
+                document.querySelector('#first-slider').value = seconds;
+            })
+            document.querySelector('#time-end').addEventListener("change", (event)=>
+            {
+                const listTime = event.target.value.split(':');
+                let seconds = (+listTime[0]) * 3600 + (+listTime[1]) * 60 + (+listTime[2]);
+                if (seconds > video.duration) 
+                {
+                    seconds = video.duration;
+                    const date = (new Date(seconds*1000 + (new Date()).getTimezoneOffset() * 60000)).toTimeString().split(' ')[0];
+                    event.target.value = date;
+                }
+                document.querySelector('#second-slider').value = seconds;
+            })
+
+            document.querySelectorAll('.slider-container input[type = range]').forEach((elem) =>
+            {
+                elem.setAttribute('max', video.duration);
+                if (elem.id == "second-slider") elem.setAttribute('value', video.duration);
+            })
+
+            const sliders = document.querySelectorAll('.slider-container input[type="range"]');
+
+            sliders[0].addEventListener('input', (event) => {
+            if (+sliders[0].value > +sliders[1].value)
+            {
+                sliders[1].value = +sliders[0].value;
+                const date = (new Date(event.target.value*1000 + (new Date()).getTimezoneOffset() * 60000)).toTimeString().split(' ')[0];
+                document.getElementById("time-start").value = date;
+                document.getElementById("time-end").value = date;
+            }
+            else
+            {
+                const date = (new Date(event.target.value*1000 + (new Date()).getTimezoneOffset() * 60000)).toTimeString().split(' ')[0];
+                document.getElementById("time-start").value = date;
+            }
+            });
+            
+            sliders[1].addEventListener('input', (event) => {
+            if (+sliders[1].value < +sliders[0].value)
+            {
+                sliders[0].value = +sliders[1].value;
+                const date = (new Date(event.target.value*1000 + (new Date()).getTimezoneOffset() * 60000)).toTimeString().split(' ')[0];
+                document.getElementById("time-start").value = date;
+                document.getElementById("time-end").value = date;
+            }
+            else
+            {
+                const date = (new Date(event.target.value*1000 + (new Date()).getTimezoneOffset() * 60000)).toTimeString().split(' ')[0];
+                document.getElementById("time-end").value = date;
+            }
+            });
+        }
         video.pause();
     }
     startEncoding.addEventListener('click', async (event) =>
@@ -25,15 +93,22 @@ window.addEventListener("load", async () =>
         const startTimeDOM = document.querySelector('#time-start');
         const endTimeDOM = document.querySelector('#time-end');
         const qualityDOM = document.querySelector('#quality-checkbox')
+        let ratioDOM;
+        document.querySelectorAll('[type=radio]').forEach(elem =>
+            {
+                if (elem.checked) ratioDOM = elem;
+            })
+        const ratio = ratioDOM.value;
+        
         const start = startTimeDOM.value;
-        const end = endTimeDOM.value ;
+        const end = endTimeDOM.value;
         
         const arrStart = start.split(':');
         const arrEnd = end.split(':');
         const duration = (parseInt(arrEnd[0])*3600 + parseInt(arrEnd[1]) * 60 + parseInt(arrEnd[2])) - (parseInt(arrStart[0])*3600 + parseInt(arrStart[1]) * 60 + parseInt(arrStart[2]));
         
 
-        const useSimple = qualityDOM.checked;
+        const useRender = qualityDOM.checked;
         let cutVideo;
         async function progressCallback(progress)
         {   
@@ -60,18 +135,12 @@ window.addEventListener("load", async () =>
         
         videoContainer.classList.remove('active');
         progressMain.classList.add('active');
-        if (useSimple) cutVideo = await executeFFmpegCommand(fileInput.files[0], `-ss ${start} -to ${end} -i input.mp4 -vcodec copy -acodec copy output.mp4`, progressCallback);
-        else cutVideo = await executeFFmpegCommand(fileInput.files[0], `-ss ${start} -to ${end} -i input.mp4 output.mp4`, progressCallback);
-        // const cutVideo = await executeFFmpegCommand(fileInput.files[0], "-ss 00:00:10 -t 10 -i input.mp4 -vcodec copy -acodec copy output.mp4");
-        // const cutVideo = await executeFFmpegCommand(fileInput.files[0], "-ss 00:00:03 -t 00:00:05 -i input.mp4 output.mp4");
+        if (!useRender) cutVideo = await executeFFmpegCommand(fileInput.files[0], `-ss ${start} -to ${end} -i input.mp4 -aspect ${ratio} -vcodec copy -acodec copy output.mp4`, progressCallback);
+        else cutVideo = await executeFFmpegCommand(fileInput.files[0], `-ss ${start} -to ${end} -i input.mp4 -aspect ${ratio} output.mp4`, progressCallback);
         video.src = URL.createObjectURL(cutVideo);
         console.log(video.src);
         video.onloadeddata = () =>
         {
-            const a = document.createElement('a');
-            a.href = video.src;
-            a.download = 'output.mp4';
-            a.click();
             videoContainer.classList.add('active');
             progressMain.classList.remove('active');
             progressBar.style["width"] = "0.0%";
@@ -102,7 +171,7 @@ async function executeFFmpegCommand(BinaryData, command, progressCallback)
             //const resFile = new File([result], 'video.mp4', {type : "video/mp4"});
             //console.log(resFile)
             //ffmpeg.FS('unlink', 'input.mp4');
-            resolve(new File([result], 'video.mp4'));
+            resolve(new File([result], 'video.mp4', {type : "video/mp4"}));
         }
         setTimeout(() => {
             reject('Time Limit Exceeded');
